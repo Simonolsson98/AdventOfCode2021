71735f90f58ef92e4734ea953c49a431c307e5de
Simon <simol@student.chalmers.se>
2021-12-10 15:04:59 +0100

    day5 working solution

--- day5.py
+++ day5.py
@@ -5,7 +5,7 @@ def init(part):
 
 	condition = bool()
 	while(i):
-		i = i[:-1]
+		i = i.rstrip("\n")
 		first_and_second = i.split(" -> ")
 		x_start = int(first_and_second[0].split(",")[0])
 		x_end = int(first_and_second[1].split(",")[0])
@@ -14,7 +14,7 @@ def init(part):
 		if(part == "1"):
 			condition = (x_start == x_end or y_start == y_end)
 		else:
-			condition = (x_start == x_end or y_start == y_end or (x_start - x_end == y_start - y_end and x_start - y_start == x_end - y_end))
+			condition = True
 			
 		if(condition):
 			lines.append(i)		
@@ -25,7 +25,6 @@ def init(part):
 lines, x_start, x_end, y_start, y_end = init("1")
 
 grid = [["x" for _ in range(1000)] for y in range(1000)]
-
 for i in range(len(lines)):
 	first_and_second = lines[i].split(" -> ")
 	x_start = int(first_and_second[0].split(",")[0])
@@ -58,12 +57,24 @@ for i in range(len(lines)):
 	x_end = int(first_and_second[1].split(",")[0])
 	y_start = int(first_and_second[0].split(",")[1])
 	y_end = int(first_and_second[1].split(",")[1])
-	for j in range(min(x_start, x_end), max(x_start, x_end) + 1):
-		for k in range(min(y_start, y_end), max(y_start, y_end) + 1):
-			if(grid[j][k] == "x"):
-				grid[j][k] = 1
-			elif(grid[j][k] == 1):
-				grid[j][k] = 2
+
+	if(x_start == x_end or y_start == y_end):
+		for j in range(min(x_start, x_end), max(x_start, x_end) + 1):
+			for k in range(min(y_start, y_end), max(y_start, y_end) + 1):
+				if(grid[j][k] == "x"):
+					grid[j][k] = 1
+				elif(grid[j][k] == 1):
+					grid[j][k] = 2
+				else:
+					pass
+	else:
+		x_delta = -1 if x_start > x_end else 1
+		y_delta = -1 if y_start > y_end else 1
+		for i, j in zip(range(x_start, x_end + x_delta, x_delta), range(y_start, y_end + y_delta, y_delta)):
+			if(grid[i][j] == "x"):
+				grid[i][j] = 1
+			elif(grid[i][j] == 1):
+				grid[i][j] = 2
 			else:
 				pass
 
@@ -72,5 +83,4 @@ sum_of_covered = 0
 for i in range(len(grid)):
 	sum_of_covered += grid[i].count(2)
 
-#not 939711, should be less
 print("solution of part 2 is: " + str(sum_of_covered))
